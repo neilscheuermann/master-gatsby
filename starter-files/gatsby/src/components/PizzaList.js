e12cfb05158676c787367b07a2d2898571797c25
Neil Scheuermann <neilscheuermann@gmail.com>
2020-10-28 07:08:17 -0600

    completed through video 21: subgrid styling

--- starter-files/gatsby/src/components/PizzaList.js
+++ starter-files/gatsby/src/components/PizzaList.js
@@ -1,25 +1,63 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+const PizzaGridStyles = styled.div`
+  display: grid;
+  /* 
+    Explanation of repeat, auto-fill vs fit, minmax(), etc...
+    https://css-tricks.com/auto-sizing-columns-css-grid-auto-fill-vs-auto-fit/  
+  */
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 4rem;
+  grid-auto-rows: auto auto 500px;
+`;
+
+const PizzaStyles = styled.div`
+  display: grid;
+  /* 
+    'subgrid' says to take your row sizing not from the 
+    PizzaStyles grid, but from the PizzaGridStyls grid
+
+    Subgrid not yet available in Chrome. Below will set up a backup.
+    Says, "try to run 'grid-template-rows: subgrid', and if it doesn't 
+    work, then run what's in the code block"
+  */
+  @supports not (grid-template-rows: subgrid) {
+    --rows: auto auto 1fr;
+  }
+  /* 
+    First checks if the variable '--rows' exists, and uses 'subgrid' if not.
+  */
+  grid-template-rows: var(--rows, subgrid);
+  grid-row: span 3;
+  gap: 1rem;
+
+  h2,
+  p {
+    margin: 0;
+  }
+`;
 
 function SinglePizza({ pizza }) {
   return (
-    <div>
+    <PizzaStyles>
       <Link to={`/pizza/${pizza.slug.current}`}>
         <h2 className="mark">{pizza.name}</h2>
-        <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
-        <Img fluid={pizza.image.asset.fluid} />
       </Link>
-    </div>
+      <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
+      <Img fluid={pizza.image.asset.fluid} />
+    </PizzaStyles>
   );
 }
 
 export default function PizzaList({ pizzas }) {
   return (
-    <div>
+    <PizzaGridStyles>
       {pizzas.map((pizza) => (
         <SinglePizza key={pizza.id} pizza={pizza} />
       ))}
-    </div>
+    </PizzaGridStyles>
   );
 }
