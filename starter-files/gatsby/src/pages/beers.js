d85df9beb9088393d029016325b5059e9888b705
Neil Scheuermann <neilscheuermann@gmail.com>
2020-12-10 08:13:51 -0700

    completed through video 27: Querying, displaying, & Styling the Beers Page

--- starter-files/gatsby/src/pages/beers.js
+++ starter-files/gatsby/src/pages/beers.js
@@ -1,9 +1,72 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import styled from 'styled-components';
 
-export default function BeersPage() {
+export default function BeersPage({ data }) {
   return (
     <>
-      <p>Hey! I'm the Beers page!</p>
+      <h2 className="center">
+        We have {data.beers.nodes.length} Beers Available. Dine in Only!
+      </h2>
+      <BeerGridStyles>
+        {data.beers.nodes.map((beer) => {
+          const rating = Math.round(beer.rating.average);
+
+          return (
+            <div className="beer" key={beer.id}>
+              <img src={beer.image} alt={`${beer.name} beer`} />
+              <h3>{beer.name}</h3>
+              {beer.price}
+              <p title={`${rating} out of 5 stars`}>
+                {'⭐️'.repeat(rating)}
+                <span style={{ filter: 'grayscale(100%)' }}>
+                  {'⭐️'.repeat(5 - rating)}
+                </span>
+                <span>({beer.rating.reviews})</span>
+              </p>
+            </div>
+          );
+        })}
+      </BeerGridStyles>
     </>
   );
 }
+
+const BeerGridStyles = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+
+  .beer {
+    border: 1px solid var(--grey);
+    padding: 2rem;
+    text-align: center;
+
+    img {
+      width: 100%;
+      height: 200px;
+      object-fit: contain;
+      display: grid;
+      align-items: center;
+      font-size: 10px;
+      color: var(--black);
+    }
+  }
+`;
+
+export const query = graphql`
+  query {
+    beers: allBeer {
+      nodes {
+        id
+        name
+        price
+        rating {
+          average
+          reviews
+        }
+        image
+      }
+    }
+  }
+`;
